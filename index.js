95502da07abe224aa4b713f2383c2454a00731ae
sayukau19 <139460409+sayukau19@users.noreply.github.com>
2023-07-24 14:02:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+//Initialize Swiper
+
+  var swiper = new Swiper(".mySwiper", {
+    speed: 600,
+    parallax: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  function goBack() {
+    window.history.back(prevEl)
+  }
+
